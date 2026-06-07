933ffb2c56fee744da08e6c0501912d13732f8ae
iamsoydev <camelcase@mygay.rodeo>
2026-06-07 11:55:39 -0400

    Completed basic player movement

--- src/player/player.gd
+++ src/player/player.gd
@@ -1,13 +1,19 @@
-extends Node2D
+extends CharacterBody2D
 
 @onready var animation_player: AnimationPlayer = $Sprite2D/AnimationPlayer
 @onready var ray_cast_2d: RayCast2D = $RayCast2D
 
+var movement_dir := Vector2.ZERO
+
+func _physics_process(delta: float) -> void:
+		velocity = movement_dir * 65
+		move_and_slide()
+
+
 func _unhandled_input(event: InputEvent) -> void:
-	var dir := Input.get_vector("walk_left","walk_right","walk_up","walk_down")
+	movement_dir = Input.get_vector("walk_left","walk_right","walk_up","walk_down")
 	#TODO: Figure out how to adjust this. Need to look at a previous project
-	position += dir *3.4
-	match(dir):
+	match(movement_dir):
 		Vector2.DOWN:
 			ray_cast_2d.rotation = deg_to_rad(0)
 		Vector2.UP: 
