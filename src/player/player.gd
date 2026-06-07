fadcd5f529b44a9953335490a9002758f4d31e47
iamsoydev <camelcase@mygay.rodeo>
2026-06-07 09:52:22 -0400

    Completed interaction raycast rotation behaviour

--- src/player/player.gd
+++ src/player/player.gd
@@ -6,5 +6,13 @@ extends Node2D
 func _unhandled_input(event: InputEvent) -> void:
 	var dir := Input.get_vector("walk_left","walk_right","walk_up","walk_down")
 	#TODO: Figure out how to adjust this. Need to look at a previous project
-	ray_cast_2d.target_position = ray_cast_2d.to_global(dir)
 	position += dir *3.4
+	match(dir):
+		Vector2.DOWN:
+			ray_cast_2d.rotation = deg_to_rad(0)
+		Vector2.UP: 
+			ray_cast_2d.rotation = deg_to_rad(180)
+		Vector2.RIGHT:
+			ray_cast_2d.rotation = deg_to_rad(-90)
+		Vector2.LEFT:
+			ray_cast_2d.rotation = deg_to_rad(90)
