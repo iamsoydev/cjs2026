bf609e1db2cf6055d06a78f01fb40859c0e0a87a
iamsoydev <camelcase@mygay.rodeo>
2026-06-10 08:38:12 -0400

    Modified Cow character to allow for more variation

--- src/actor/actor.gd
+++ src/actor/actor.gd
@@ -1,9 +1,12 @@
 class_name Actor
 extends CharacterBody2D
 
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var dialogue_node: DialogueNode = $DialogueNode
+
+
 @onready var interaction_node: InteractionNode = $InteractionNode
 @onready var interaction_cooldown_timer: Timer = $InteractionCooldownTimer
-
 
 func _on_interaction_cooldown_timer_timeout() -> void:
 	pass # Replace with function body.
